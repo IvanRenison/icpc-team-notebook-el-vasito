df1f365c32e760755470da93ae806d7822e3fc88
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-01-15 15:07:44 -0300

    Put `AhoCorasick` in a struct

--- strings/aho_corasick_test.cpp
+++ strings/aho_corasick_test.cpp
@@ -9,41 +9,44 @@
 using namespace std;
 typedef long long ll;
 
-struct vertex {
-	map<char,int> next,go;
-	int p,link;
-	char pch;
-	vector<int> leaf;
-	vertex(int p=-1, char pch=-1):p(p),pch(pch),link(-1){}
-};
-vector<vertex> t;
-void aho_init(){ //do not forget!!
-	t.clear();t.pb(vertex());
-}
-void add_string(string s, int id){
-	int v=0;
-	for(char c:s){
-		if(!t[v].next.count(c)){
-			t[v].next[c]=t.size();
-			t.pb(vertex(v,c));
+struct AhoCorasick {
+	struct vertex {
+		map<char,int> next,go;
+		int p,link;
+		char pch;
+		vector<int> leaf;
+		vertex(int p=-1, char pch=-1):p(p),link(-1),pch(pch){}
+	};
+	vector<vertex> t;
+	AhoCorasick():t(1){}
+	void add_string(string s, int id){
+		int v=0;
+		for(char c:s){
+			if(!t[v].next.count(c)){
+				t[v].next[c]=t.size();
+				t.pb(vertex(v,c));
+			}
+			v=t[v].next[c];
 		}
-		v=t[v].next[c];
+		t[v].leaf.pb(id);
 	}
-	t[v].leaf.pb(id);
-}
-int go(int v, char c);
-int get_link(int v){
-	if(t[v].link<0)
-		if(!v||!t[v].p)t[v].link=0;
-		else t[v].link=go(get_link(t[v].p),t[v].pch);
-	return t[v].link;
-}
-int go(int v, char c){
-	if(!t[v].go.count(c))
-		if(t[v].next.count(c))t[v].go[c]=t[v].next[c];
-		else t[v].go[c]=v==0?0:go(get_link(v),c);
-	return t[v].go[c];
-}
+	int get_link(int v){
+		if(t[v].link<0) {
+			if(!v||!t[v].p)t[v].link=0;
+			else t[v].link=go(get_link(t[v].p),t[v].pch);
+		}
+		return t[v].link;
+	}
+	int go(int v, char c){
+		if(!t[v].go.count(c)) {
+			if(t[v].next.count(c))t[v].go[c]=t[v].next[c];
+			else t[v].go[c]=v==0?0:go(get_link(v),c);
+		}
+		return t[v].go[c];
+	}
+};
+
+AhoCorasick ac;
 
 bool r[1024];
 bool vis[1<<20];
@@ -51,8 +54,8 @@ bool vis[1<<20];
 void proc(int x){
 	if(x==-1||vis[x])return;
 	vis[x]=true;
-	fore(i,0,t[x].leaf.size())r[t[x].leaf[i]]=true;
-	proc(t[x].link);
+	fore(i,0,ac.t[x].leaf.size())r[ac.t[x].leaf[i]]=true;
+	proc(ac.t[x].link);
 }
 
 char s[100005];
@@ -63,15 +66,15 @@ int main(){
 	int tn;
 	scanf("%d",&tn);
 	while(tn--){
-		aho_init();
+		ac = AhoCorasick();
 		scanf("%s%d",s,&n);
 		fore(i,0,n){
 			scanf("%s",tt);
-			add_string(string(tt),i);
+			ac.add_string(string(tt),i);
 		}
 		int k=0;
 		for(int i=0;s[i];++i){
-			k=go(k,s[i]);
+			k=ac.go(k,s[i]);
 			proc(k);
 		}
 		fore(i,0,n)puts(r[i]?"y":"n");
